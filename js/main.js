07644fa1355587f31773d9ffae01fa2678646338
LuisLeon-Dev <lleon5073@gmail.com>
2023-12-06 14:17:10 -0600

    refactoring

--- js/main.js
+++ js/main.js
@@ -3,14 +3,19 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
-
-btnCloseModal.addEventListener("click", function () {
+function closeModal() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-});
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
